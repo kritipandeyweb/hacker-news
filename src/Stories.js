2e8715f23057064c8f7344de199b5aa0bd3845d8
Kriti Pandey <78684677+kritipandeyweb@users.noreply.github.com>
2023-02-03 18:30:39 +0530

    setup basic structure

---
+++ src/Stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { useGlobalContext } from "./context";
+
+const Stories = () => {
+  const { isLoading } = useGlobalContext();
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+};
+
+export default Stories;
