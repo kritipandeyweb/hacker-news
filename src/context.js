23a178bbb3f958c0c17c7cc4712cdb5328b80269
Kriti Pandey <78684677+kritipandeyweb@users.noreply.github.com>
2023-02-06 11:39:53 +0530

    added a view story feature

--- src/context.js
+++ src/context.js
@@ -13,6 +13,10 @@ const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
 
 const initialState = {
   isLoading: true,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
 };
 
 const AppContext = React.createContext();
@@ -21,9 +25,17 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const fetchStories = async (url) => {
     dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {}
   };
   useEffect(() => {
-    fetchStories();
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, []);
   return (
     <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
